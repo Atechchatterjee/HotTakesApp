19c87d30ae99ae195f241400b4e145f54a514d3d
Atechchatterjee <atechchatterjeee@gmail.com>
2023-05-23 19:23:23 +0530

    style: add colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg_primary: "#120D31",
+        secondary: "#211E3F",
+        primary: "#4A41AA",
+        accent: "#B6AFFF",
+        inactive: "#D8D8D8",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
