e76781eeadd8b6d7706d2e941308dc3f3f331dca
Atechchatterjee <atechchatterjeee@gmail.com>
2023-06-04 21:57:09 +0530

    feat: add database id

--- utils/appwriteConfig.ts
+++ utils/appwriteConfig.ts
@@ -5,6 +5,7 @@ export const appwriteClient = new Client()
   .setEndpoint(env.NEXT_PUBLIC_APPWRITE_PROJECT_ENDPOINT)
   .setProject(env.NEXT_PUBLIC_APPWRITE_PROJECT_ID ?? "");
 
+export const hottakesDatabaseId = "646eed2e4349377fd47a";
 export const appwriteDatabase = new Databases(appwriteClient);
 export const appwriteAccount = new Account(appwriteClient);
 export const appwriteAvatars = new Avatars(appwriteClient);
