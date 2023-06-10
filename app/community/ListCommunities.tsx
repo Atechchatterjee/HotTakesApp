d0f7fc91f7ede13cc482bd1a6c1c345ec53f9d22
Atechchatterjee <atechchatterjeee@gmail.com>
2023-06-10 07:45:58 +0530

    refactor: put list community component in seperate file

---
+++ app/community/ListCommunities.tsx
@@ -0,0 +1,137 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useStore } from "store";
+import { inter } from "app/fonts";
+import { appwriteDatabase, hottakesDatabaseId } from "utils/appwriteConfig";
+import { Models, Query } from "appwrite";
+import Collections from "utils/appwriteCollections";
+import { Button } from "app/components/ui/button";
+import { ExternalLink, Users } from "lucide-react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { useRouter } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+
+interface ListCommunitiesProps {
+  communities: Models.Document[];
+  setCommunities: React.Dispatch<React.SetStateAction<Models.Document[]>>;
+}
+
+export default function ListCommunities({
+  communities,
+  setCommunities,
+}: ListCommunitiesProps) {
+  const [parent, enableAnimations] = useAutoAnimate();
+  const [joinedCommunitySet, setJoinedCommunitySet] = useState<Set<string>>(
+    new Set()
+  );
+  const [communityToJoin, setCommunityToJoin] = useState<Models.Document>();
+
+  const { userId } = useStore((state) => state.user);
+
+  const router = useRouter();
+
+  useQuery({
+    queryFn: async () => {
+      const { documents } = await appwriteDatabase.listDocuments(
+        hottakesDatabaseId,
+        Collections["Communities"]
+      );
+      setCommunities(documents);
+    },
+  });
+
+  useQuery({
+    queryKey: [userId, communities],
+    queryFn: async () => {
+      const { documents: _communities } = await appwriteDatabase.listDocuments(
+        hottakesDatabaseId,
+        Collections["Community Relations"],
+        [Query.equal("userId", [userId])]
+      );
+
+      const newJoined = new Set<string>();
+
+      // checks which communities the current user has joined
+      communities.forEach((community) => {
+        _communities.forEach((_community) => {
+          if (community.$id === _community.communities[0].$id) {
+            newJoined.add(community.$id);
+          }
+        });
+      });
+
+      setJoinedCommunitySet(newJoined);
+    },
+  });
+
+  const { refetch: joinCommunity } = useQuery({
+    queryFn: async () => {
+      await appwriteDatabase.createDocument(
+        hottakesDatabaseId,
+        Collections["Community Relations"],
+        "",
+        {
+          userId,
+          communities: [communityToJoin?.$id],
+        }
+      );
+      setJoinedCommunitySet(
+        (prevState) => new Set([...prevState, communityToJoin?.$id || ""])
+      );
+    },
+  });
+
+  useEffect(() => {
+    enableAnimations(true);
+  }, []);
+
+  return (
+    <div className="mt-5 flex flex-col gap-4" ref={parent}>
+      <p className="text-sm font-medium text-gray-400">LIST OF COMMUNITIES</p>
+      {communities.map((community, i) => {
+        return (
+          <div
+            className="flex flex-col gap-2 rounded-lg border border-btn_secondary bg-secondary pb-5 pl-7 pr-7 pt-5 transition-all duration-300 hover:brightness-[130%]"
+            key={i}
+          >
+            <h2 className={`${inter.className} text-2xl font-bold`}>
+              {community.name}
+            </h2>
+            <p className="text-[1.1rem] font-medium">
+              {community.authorName}
+              <span className="font-bold text-accent"> @author</span>
+            </p>
+            <p className="text-gray-400">{community.description}</p>
+            <div className="mt-7 flex w-[18rem] gap-3">
+              <Button
+                variant="primary"
+                className={"flex-1 gap-2"}
+                disabled={
+                  joinedCommunitySet.has(community.$id) ||
+                  userId === community.author
+                }
+                onClick={() => {
+                  setCommunityToJoin(community);
+                  joinCommunity();
+                }}
+              >
+                <Users size="0.9rem" />
+                {joinedCommunitySet.has(community.$id) ? "Joined" : "Join"}
+              </Button>
+              <Button
+                variant="secondary"
+                className="flex-1 gap-2"
+                onClick={() => {
+                  router.push(`/community/${community.$id}`);
+                }}
+              >
+                <ExternalLink size="1rem" />
+                Details
+              </Button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,135 +1,17 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import WithAuth from "app/components/WithAuth";
-import { useStore } from "store";
 import { inter } from "app/fonts";
 import { RefetchContext } from "context/RefetchContext";
 import { appwriteDatabase, hottakesDatabaseId } from "utils/appwriteConfig";
 import { Models, Query } from "appwrite";
 import Collections from "utils/appwriteCollections";
 import { Button } from "app/components/ui/button";
-import { ExternalLink, Search, Users } from "lucide-react";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
+import { Search } from "lucide-react";
 import { Input } from "app/components/ui/input";
 import { SelectSeparator } from "app/components/ui/select";
 import UIWrapper from "app/components/UIWrapper";
-import { join } from "path";
-
-interface ListCommunitiesProps {
-  communities: Models.Document[];
-  setCommunities: React.Dispatch<React.SetStateAction<Models.Document[]>>;
-}
-
-function ListCommunities({
-  communities,
-  setCommunities,
-}: ListCommunitiesProps) {
-  const [parent, enableAnimations] = useAutoAnimate();
-  const [joined, setJoined] = useState<Set<string>>(new Set());
-  const { userId } = useStore((state) => state.user);
-
-  async function fetchCommunities() {
-    try {
-      const { documents } = await appwriteDatabase.listDocuments(
-        hottakesDatabaseId,
-        Collections["Communities"]
-      );
-      setCommunities(documents);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const { documents: _communities } =
-          await appwriteDatabase.listDocuments(
-            hottakesDatabaseId,
-            Collections["Community Relations"],
-            [Query.equal("userId", [userId])]
-          );
-
-        const newJoined = new Set<string>();
-
-        communities.forEach((community) => {
-          _communities.forEach((_community) => {
-            if (community.$id === _community.communities[0].$id) {
-              newJoined.add(community.$id);
-            }
-          });
-        });
-        setJoined(newJoined);
-      } catch (err) {
-        console.error(err);
-      }
-    })();
-  }, [userId, communities]);
-
-  async function joinCommunities(community: Models.Document) {
-    try {
-      await appwriteDatabase.createDocument(
-        hottakesDatabaseId,
-        Collections["Community Relations"],
-        "",
-        {
-          userId,
-          communities: [community.$id],
-        }
-      );
-      setJoined((prevState) => new Set([...prevState, community.$id]));
-    } catch (err) {
-      console.error(err);
-    }
-  }
-
-  useEffect(() => {
-    fetchCommunities();
-    enableAnimations(true);
-  }, []);
-
-  return (
-    <div className="mt-5 flex flex-col gap-4" ref={parent}>
-      <p className="text-sm font-medium text-gray-400">LIST OF COMMUNITIES</p>
-      {communities.map((community, i) => {
-        return (
-          <div
-            className="flex flex-col gap-2 rounded-lg border border-btn_secondary bg-secondary pb-5 pl-7 pr-7 pt-5 transition-all duration-300 hover:brightness-[130%]"
-            key={i}
-          >
-            <h2 className={`${inter.className} text-2xl font-bold`}>
-              {community.name}
-            </h2>
-            <p className="text-[1.1rem] font-medium">
-              {community.authorName}
-              <span className="font-bold text-accent"> @author</span>
-            </p>
-            <p className="text-gray-400">{community.description}</p>
-            <div className="mt-7 flex w-[18rem] gap-3">
-              <Button
-                variant="primary"
-                className={"flex-1 gap-2"}
-                disabled={
-                  joined.has(community.$id) || userId === community.author
-                }
-                onClick={() => {
-                  joinCommunities(community);
-                }}
-              >
-                <Users size="0.9rem" />
-                {joined.has(community.$id) ? "Joined" : "Join"}
-              </Button>
-              <Button variant="secondary" className="flex-1 gap-2">
-                <ExternalLink size="1rem" />
-                Know More
-              </Button>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+import ListCommunities from "./ListCommunities";
 
 function Community() {
   const [communities, setCommunities] = useState<Models.Document[]>([]);
@@ -172,6 +54,11 @@ function Community() {
               placeholder="Find your community"
               className="h-full border border-btn_secondary bg-secondary hover:brightness-[130%] focus:border focus:border-primary focus:brightness-[130%]"
               onChange={(e: any) => setSearch(e.target.value)}
+              onKeyDownCapture={(e) => {
+                if (e.code === "Enter") {
+                  searchCommunityByTopic();
+                }
+              }}
             />
             <Button
               variant="primary"
