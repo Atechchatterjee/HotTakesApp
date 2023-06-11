b62c3c1fd01a0a9bcb1874051436e311697931de
Atechchatterjee <atechchatterjeee@gmail.com>
2023-06-11 22:32:51 +0530

    feat: configure express server for web sockets

---
+++ web-socket-server/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+server.listen(4000);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected ", socket.id);
+  socket.on("hello", (msg) => {
+    console.log({ msg });
+  });
+  socket.on("challenged", ({ challenge }) => {
+    socket.broadcast.emit("challenge-broadcast", {
+      challenge,
+    });
+  });
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+  socket.on(
+    "send-challenge-message",
+    ({ currentChallenge, challengeMessage }) => {
+      socket.broadcast.emit("broadcast-challenge-message", {
+        currentChallenge,
+        challengeMessage,
+      });
+      console.log({ currentChallenge, challengeMessage });
+    }
+  );
+});
