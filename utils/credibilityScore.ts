b4810302cd76a31eb15e57ae683eed78bb9b7bc9
Atechchatterjee <atechchatterjeee@gmail.com>
2023-06-13 20:38:29 +0530

    feat: list out amout of credibillity increment

---
+++ utils/credibilityScore.ts
@@ -0,0 +1,4 @@
+export const credibilityIncrements = {
+  incParticipation: 1, // increase in credibility score if a user participates in discussion
+  incDiscussion: 2, // increase in credibility score if a user gives in own opinions in a discussion topic
+};
